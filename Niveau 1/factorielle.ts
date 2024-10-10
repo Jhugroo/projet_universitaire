a8d7d6863534244698753dac90ddcde80658c53d
felania-f <felania71@gmail.com>
2024-10-10 15:27:52 +0200

    Initial commit

--- Niveau 1/factorielle.ts
+++
@@ -1,9 +0,0 @@
-import * as readlineSync from 'readline-sync';
-
-function trouverFactorielle(num: number): number {
-  if (num === 0) return 1;
-  return num * trouverFactorielle(num - 1);
-}
-
-let factorielleResult: number = Number(readlineSync.question("Entrez un nombre: "));
-console.log(`La factorielle ${factorielleResult} est: ${trouverFactorielle(factorielleResult)}`);
