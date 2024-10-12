99096289088f6a33270fb6e5625b930a7092a7da
Jhugroo <prakash.jhugroo@gmail.com>
2024-10-12 21:12:59 +0400

    feature/cleanup Added class

--- Niveau 0/nbPair.ts
+++
@@ -1,9 +0,0 @@
-import * as readlineSync from 'readline-sync';
-
-function is_even(a: number): boolean {
-  return a % 2 === 0;
-}
-let a1: number = Number(readlineSync.question("Entrez nb:"));
-
-const resultEven = is_even(a1);
-console.log(resultEven ? `${a1} est pair` : `${a1} est impair`);
